e34e97b0242b29a54b5d6750a522149fbbe0ec3a
ravi31srv <ravi31srv@gmail.com>
2023-06-15 14:20:21 +0530

    Applied global validationpipe , added swagegr configuration for documentation to botstrap the app instance with required configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as dotenv from "dotenv";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle("BiteSpeed")
+    .setDescription("BiteSpeed API which gives contact information in response")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("Doc", app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT);
 }
 bootstrap();
