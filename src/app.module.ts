ba3afaba1f1763518fab11d2c5e2736d24d84ff3
ravi31srv <ravi31srv@gmail.com>
2023-06-15 14:25:43 +0530

    mysql database conencted and made availabel to use within other services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { contacts } from './contact.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+    
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username:process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [contacts]
+
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [TypeOrmModule]
+
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+  async onModuleInit() {
+    console.log("db connected successfully")
+  }
+
+
+}
