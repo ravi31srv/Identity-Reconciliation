2db0e0c96f7bdb35d1b8ddd52f84a70764f04e31
ravi31srv <ravi31srv@gmail.com>
2023-06-15 14:28:41 +0530

    Added 'identify' route to access POST API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { identificationDto } from './dtos/identification.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
+  getHello(): Promise<any> {
     return this.appService.getHello();
   }
+
+
+  @Post('/identify')
+  identify(@Body() identificationBody: identificationDto): Promise<any> {
+    return this.appService.identifyService(identificationBody)
+
+
+  }
+
 }
